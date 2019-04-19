2957043bf2c0f844c6c157e16c17383b30728909
nchernia <Nikita.Cherniakov@t-systems.ru>
2019-04-19 15:25:20 +0300

    Config Auth0

---
+++ src/services/auth/auth.config.ts
@@ -0,0 +1,8 @@
+export const AUTH_CONFIG = {
+    // Needed for Auth0 (capitalization: ID):
+    clientID: '134V3XHYwqqwN0mKwsY03Hy7hnpw1qsP',
+    // Needed for Auth0Cordova (capitalization: Id):
+    clientId: '134V3XHYwqqwN0mKwsY03Hy7hnpw1qsP',
+    domain: 'dev-puzzle-team.eu.auth0.com',
+    packageIdentifier: 'instabot' // config.xml widget ID, e.g., com.auth0.ionic
+};
